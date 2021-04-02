2d9af3c26e8087b94169ff3b3c46837c3d8e1936
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-04-02 10:20:33 +0100

    Add scraper function

--- scraper.rb
+++ scraper.rb
@@ -1,1 +1,12 @@
+require 'nokogiri'
+require 'httparty'
+require 'byebug'
 
+def scraper
+    url = "https://www.lupus.org/"
+    unparsed_page = HTTParty.get(url)
+    parsed_page = Nokogiri::HTML(unparsed_page)
+    byebug
+end
+
+scraper
