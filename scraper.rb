4f4b87166101cfc11387fe3ff4f426cb2a37f8e7
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-04-05 16:23:10 +0100

    Correct linter error

--- scraper.rb
+++ scraper.rb
@@ -6,13 +6,10 @@ def scraper
   url = 'https://www.lupus.org/'
   unparsed_page = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page)
-  news_wrap = parsed_page.css(".news-resources-promo")[1]
+  news_wrap = parsed_page.css('.news-resources-promo')[1]
   news_items = news_wrap.css('.news-resources-promo-item')
-  headline = news_items.map { |item| item.at_css('.news-resources-promo-item-title').text }
+  content = news_items.map { |item| item.at_css('.news-resources-promo-item-title').text }
   byebug
-  
 end
 
-
 scraper
-
