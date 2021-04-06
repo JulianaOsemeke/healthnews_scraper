7febff024473513cb1aac5851caf34c1ba44e45f
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-04-06 10:29:53 +0100

    Add lib directory

---
+++ lib/scraper.rb
@@ -0,0 +1,14 @@
+require 'byebug'
+require 'httparty'
+require 'nokogiri'
+
+class NewsScraper
+  def scrap
+    url = 'https://www.lupus.org/'
+    unparsed_page = HTTParty.get(url)
+    parsed_page = Nokogiri::HTML(unparsed_page)
+    news_wrap = parsed_page.css('.news-resources-promo')[1]
+    news_items = news_wrap.css('.news-resources-promo-item')
+    news_items.map { |item| item.at_css('.news-resources-promo-item-title').text }
+  end
+end
