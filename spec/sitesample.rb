38cdd9eee2c3610044ec404fc70a19b99996231d
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-04-06 16:24:21 +0100

    Add sample folder to spec folder

---
+++ spec/sitesample.rb
@@ -0,0 +1,16 @@
+SAMPLE = %{
+  <main>
+    <div class="news-resources-promo"></div>
+    <div class="news-resources-promo">
+      <div class="news-resources-promo-items">
+        <a class="news-resources-promo-item">
+          <div class="news-resources-promo-item-title h4">
+            Lupus Foundation of America New Three-Year Grant Supports Research to
+            Improve Pregnancy Outcomes in Women with Lupus
+          </div>
+          <div class="news-resources-promo-item-type">News</div>
+        </a>
+      </div>
+    </div>
+  </main>
+}     
