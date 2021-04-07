ee751dbf47f8c34487c5cd523a581074fe742a1a
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-04-07 21:57:45 +0100

    Correct linter errors

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,10 +8,10 @@ class NewsScraper
       page = browser.pages.first || browser.new_page
       page.goto('https://www.lupus.org/news')
       page.query_selector_all('.filtered-content-item-content').map do |item|
-      title = item.eval_on_selector('.filtered-content-item-title', 'div => div.innerText')
-      summary = item.eval_on_selector('.filtered-content-item-summary', 'div => div.innerText')
-      date = item.eval_on_selector('.filtered-content-item-date', 'div => div.innerText')
-      NewsItem.new(title, summary, date)
+        title = item.eval_on_selector('.filtered-content-item-title', 'div => div.innerText')
+        summary = item.eval_on_selector('.filtered-content-item-summary', 'div => div.innerText')
+        date = item.eval_on_selector('.filtered-content-item-date', 'div => div.innerText')
+        NewsItem.new(title, summary, date)
       end
     end
   end
