23d00801cc081f11af602a975b0bfa02de690f75
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-04-07 18:00:37 +0100

    Update tests

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -7,8 +7,19 @@ describe NewsScraper do
     let(:scraper) { NewsScraper.new }
 
     it 'returns all the news summaries on a page' do
-      stub_request(:get, 'https://www.lupus.org/').to_return(status: 200, body: SAMPLE)
-      expect(scraper.scrap.size).to be 1
+      stub_request(:get, 'https://www.lupus.org/news').to_return(status: 200, body: SAMPLE)
+      expect(scraper.scrap.size).to eq (12)
     end
+
+    it 'correctly returns the title of a news item' do
+      stub_request(:get, 'https://www.lupus.org/news').to_return(status: 200, body: SAMPLE)
+      expect(scraper.scrap[0].title).to eq ('Stroke Outcomes in People with Lupus')
+    end
+
+    it 'correctly returns the date of a news item' do
+      stub_request(:get, 'https://www.lupus.org/news').to_return(status: 200, body: SAMPLE)
+      expect(scraper.scrap[0].date).to eq ('April 7, 2021')
+    end
+
   end
 end
