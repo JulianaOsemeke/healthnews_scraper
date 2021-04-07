8b494c5c69a8e69ca4736eb4abc6a8a3fb600aaf
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-04-07 18:04:23 +0100

    Edit site sample div classes

--- spec/sitesample.rb
+++ spec/sitesample.rb
@@ -1,16 +1,25 @@
 SAMPLE = %(
   <main>
-    <div class="news-resources-promo"></div>
-    <div class="news-resources-promo">
-      <div class="news-resources-promo-items">
-        <a class="news-resources-promo-item">
-          <div class="news-resources-promo-item-title h4">
-            Lupus Foundation of America New Three-Year Grant Supports Research to
-            Improve Pregnancy Outcomes in Women with Lupus
+  <div>
+      <div>
+      <a class="filtered-content-item" href="/news/lupus-foundation-grant-supports-research-to-improve-pregnancy-outcomes">
+          <div class="filtered-content-item-image">
+            <div class="filtered-content-item-image-inner" style="background-image: url('https://s3.amazonaws.com/files.lupus.org/styles/search/s3/public/images/resources/Pregnancy_IMPACT-study.jpg?h=313d1aca&amp;itok=eSY-zQOj')"></div>
+            <span class="filtered-content-item-label" data-label="new">New</span>
           </div>
-          <div class="news-resources-promo-item-type">News</div>
+          <div class="filtered-content-item-content">
+            <div class="filtered-content-item-content-main" style="height: 160px;">
+              <div class="filtered-content-item-title">Lupus Foundation of America New Three-Year Grant Supports Research to  Improve Pregnancy Outcomes in Women with Lupus</div>
+              <div class="filtered-content-item-date">March 31, 2021</div>
+              <div class="filtered-content-item-summary">A new three-year grant, funded by the Festa Family will support improving pregnancy outcomes in high...</div>
+            </div>
+            <div class="filtered-content-item-type">News</div>
+          </div>
         </a>
+
       </div>
     </div>
+  
   </main>
 ).freeze
+    
